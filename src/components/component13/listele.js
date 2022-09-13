b0b28859167b3b51ce15306a3a64ce05f7111f4a
Feridun Ozcelik <feridun@Feriduns-MBP.home>
2022-09-13 20:47:35 +0300

    13sep2022

--- src/components/component13/listele.js
+++ src/components/component13/listele.js
@@ -3,12 +3,29 @@ import data from './data'
 import List from './list';
 const Listele = () => {
     const [people,setPeople]=useState(data);
+    const [resetAll,setResetAll] = useState(false);
+
+    const clearAll = () => { 
+      setPeople([]);
+      setResetAll(true);
+     }
+
+    function refreshPage() {
+      setPeople(data);
+      setResetAll(false);
+    }
+
+
   return (
     <main>
         <section className="container">
             <h3>{people.length} birthdays today</h3>
             <List people={people}/>
-            <button onClick={()=>setPeople([])}>Clear All</button>
+            {
+              !resetAll ? (<button onClick={clearAll}>Clear All</button>) : <button onClick={refreshPage}>Refresh Page</button>
+            }
+            
+            
         </section>
     </main>
   )
